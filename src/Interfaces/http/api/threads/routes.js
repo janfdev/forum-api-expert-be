ecc349069f653083c1db956aa3d3b01d0dd5f638
janfdev <fauzanrizqinoor@gmail.com>
2026-04-22 02:22:22 +0700

    feat: add thread routes and documentation for deployment and submission requirements

--- src/Interfaces/http/api/threads/routes.js
+++ src/Interfaces/http/api/threads/routes.js
@@ -3,7 +3,9 @@ import express from 'express';
 const createThreadsRouter = (handler) => {
   const router = express.Router({ mergeParams: true });
 
-  router.post('/', handler.postThreadHandler);
+  router.post('/',
+
+    handler.postThreadHandler);
   router.get('/:threadId', handler.getThreadDetailHandler);
 
   return router;
